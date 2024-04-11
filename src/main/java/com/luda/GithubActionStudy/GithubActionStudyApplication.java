b2785df47e027436609efd88d422d27b0b937b79
luda412 <201810791@sangmyung.kr>
2024-04-11 11:01:18 +0900

    init commit

---
+++ src/main/java/com/luda/GithubActionStudy/GithubActionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.luda.GithubActionStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luda/GithubActionStudy/GithubActionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luda.GithubActionStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
